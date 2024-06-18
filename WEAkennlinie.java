67fccc830a349dcfc9206b9d679cb5709e5a9499
Leif <llj.bremer@gmail.com>
2024-06-18 11:13:40 +0200

    Funktioniert nicht lol

--- WEAkennlinie.java
+++ WEAkennlinie.java
@@ -1,3 +1,8 @@
+import java.util.TreeMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Beschreiben Sie hier die Klasse WEAkennlinie.
@@ -7,9 +12,31 @@
  */
 public class WEAkennlinie
 {
+    private final String StandartDatei = "WEA-Kennlinienmodell_Beispiel.txt";
+
+    private TreeMap<Double,Double> WEAkennline;
+
     public WEAkennlinie ()
     {
-        
+        TXTDateiEinlesen();
     }
-    
+
+    private void TXTDateiEinlesen()
+    {
+        try {BufferedReader reader = new BufferedReader(
+                    new FileReader(StandartDatei));
+            String zeile = reader.readLine();
+            while(zeile != null){
+                zeile = reader.readLine();
+                String[] zeileArray = zeile.split(" ");
+                WEAkennline.put(new Double(zeileArray[0]),new Double(zeileArray[1]));
+            }
+        }
+        catch(FileNotFoundException e){
+
+        }
+        catch(IOException e){
+
+        }
+    }
 }
