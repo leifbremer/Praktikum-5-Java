9329165d83152142c797c2b4c2718c131dcf767a
Nicolas Lyer <lyer.nicolas@gmail.com>
2024-06-18 10:21:31 +0200

    Klasse hinzugefügt

---
+++ WEAkennlinie.java
@@ -0,0 +1,15 @@
+
+/**
+ * Beschreiben Sie hier die Klasse WEAkennlinie.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class WEAkennlinie
+{
+    public WEAkennlinie ()
+    {
+        
+    }
+    
+}
